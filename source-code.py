b14fb95e68ff767db96374f30eb1aeb6418cce9b
farhatizakaria <zakariafarhati18@gmail.com>
2019-12-06 16:30:34 +0000

    Update source-code.py
    Improve the script

--- source-code.py
+++ source-code.py
@@ -1,9 +1,46 @@
+from time import sleep
 class Cars:
-    
-    def increment(self):
-        print("Increment")
-    
-    def decrement(self):
-        print("Decrement")
 
-user_car = Cars()
+    def __init__(self,name,speed,fuel):
+        print("Getting Data! ")
+        self.name = name
+        self.speed = speed
+        self.fuel = fuel
+
+        if type(self.name) != str or self.name == "":
+            print("You didn't write a name! ")
+            str("Press any key to exit ")
+            exit()
+        else:
+            pass
+
+    
+    def increment(self,count):
+        print("Incrementing the speed... ")
+        sleep(3)
+        while self.speed < count:
+            self.speed = self.speed + 5
+            if self.speed == count:
+                print("The speed is good according what you want!")
+                print(self.speed)
+                break #Stop the loop
+            else:
+                continue #Continue the loop :) until gets the correct value 
+
+    
+    def decrement(self,count):
+        print("Decrementing the speed... ")
+        sleep(3)
+        while self.speed > count:
+            self.speed = self.speed - 5
+            if self.speed == count:
+                print("The value of speed as you want!")
+                print(self.speed)
+                break
+            else:
+                continue
+
+
+#Attributes
+user_car = Cars("Dacia",210,1)
+user_car.increment(270)
